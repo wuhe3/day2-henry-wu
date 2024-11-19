9aad77f5ba457cc4763934d1f06d55cae884e844
WUHE3 <wuhe3@oocl.com>
2024-11-19 11:34:24 +0800

    [MultiplicationTable] test, 1. 'should_return_false_when_isValid_given_2_and_1' 2. 'should_return_false_when_isValid_given_0_and_2' 3. 'should_return_false_when_isValid_given_2_and_1001' 4. 'should_return_false_when_isValid_given_1001_and_2'

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -93,6 +93,7 @@ class MultiplicationTableBuilderTest {
     // 2,1 f
     // 0,2 f
     // 2,1001 f
+    // 1001,2 f
     @Test
     void should_return_true_when_isValid_given_2_and_3() {
         // Given
@@ -105,10 +106,63 @@ class MultiplicationTableBuilderTest {
 
         // Then
         assertTrue(isValid);
-
     }
 
+    @Test
+    void should_return_false_when_isValid_given_2_and_1() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 2;
+        int end = 1;
 
+        // When
+        boolean isValid = multiplicationTableBuilder.isValid(start, end);
+
+        // Then
+        assertFalse(isValid);
+    }
+
+    @Test
+    void should_return_false_when_isValid_given_0_and_2() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 0;
+        int end = 2;
+
+        // When
+        boolean isValid = multiplicationTableBuilder.isValid(start, end);
+
+        // Then
+        assertFalse(isValid);
+    }
+
+    @Test
+    void should_return_false_when_isValid_given_2_and_1001() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 2;
+        int end = 1001;
+
+        // When
+        boolean isValid = multiplicationTableBuilder.isValid(start, end);
+
+        // Then
+        assertFalse(isValid);
+    }
+
+    @Test
+    void should_return_false_when_isValid_given_1001_and_2() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 1001;
+        int end = 2;
+
+        // When
+        boolean isValid = multiplicationTableBuilder.isValid(start, end);
+
+        // Then
+        assertFalse(isValid);
+    }
 
 
 }
