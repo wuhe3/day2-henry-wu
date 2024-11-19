6fb9a99530fd071dfd803a2947fb06560ccc4e05
WUHE3 <wuhe3@oocl.com>
2024-11-19 11:01:37 +0800

    [MultiplicationTable] feat, 'isStarterNotBiggerThanEnd'

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -4,4 +4,8 @@ public class MultiplicationTableBuilder {
     public boolean isInRange(int number) {
         return number > 0 && number <= 1000;
     }
+
+    public boolean isStarterNotBiggerThanEnd(int start, int end) {
+        return start <= end;
+    }
 }
