86cae330d69dec88ed30517494dc53df4cfb9585
WUHE3 <wuhe3@oocl.com>
2024-11-19 11:08:28 +0800

    [MultiplicationTable] test, 'should_return_false_when_isStarterNotBiggerThanEnd_given_3_and_2' and 'should_return_true_when_isStarterNotBiggerThanEnd_given_3_and_3'

--- src/test/java/com/tw/MultiplicationTableBuilderTest.java
+++ src/test/java/com/tw/MultiplicationTableBuilderTest.java
@@ -61,6 +61,34 @@ class MultiplicationTableBuilderTest {
         assertTrue(isStarterNotBiggerThanEnd);
     }
 
+    @Test
+    void should_return_false_when_isStarterNotBiggerThanEnd_given_3_and_2() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 3;
+        int end = 2;
+
+        // When
+        boolean isStarterNotBiggerThanEnd = multiplicationTableBuilder.isStarterNotBiggerThanEnd(start, end);
+
+        // Then
+        assertFalse(isStarterNotBiggerThanEnd);
+    }
+
+    @Test
+    void should_return_true_when_isStarterNotBiggerThanEnd_given_3_and_3() {
+        // Given
+        MultiplicationTableBuilder multiplicationTableBuilder = new MultiplicationTableBuilder();
+        int start = 3;
+        int end = 3;
+
+        // When
+        boolean isStarterNotBiggerThanEnd = multiplicationTableBuilder.isStarterNotBiggerThanEnd(start, end);
+
+        // Then
+        assertTrue(isStarterNotBiggerThanEnd);
+    }
+
 
 
 }
