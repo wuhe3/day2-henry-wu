cb42a4976d4d026c8d68c5556f3233ac1345c342
WUHE3 <wuhe3@oocl.com>
2024-11-19 11:51:18 +0800

    [MultiplicationTable] feat, 1. generateTable() 2. printMultiplicationTable() 3. generateMultiplicationLine()

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -12,4 +12,31 @@ public class MultiplicationTableBuilder {
     public boolean isValid(int startNum, int endNum) {
         return isStarterNotBiggerThanEnd(startNum, endNum) && isInRange(startNum) && isInRange(endNum);
     }
+
+    public String generateTable(int startNum, int endNum) {
+        if (!isValid(startNum, endNum)) {
+            return null;
+        } else {
+            return printMultiplicationTable(startNum, endNum);
+        }
+    }
+
+    public String printMultiplicationTable(int startNum, int endNum) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = startNum; i <= endNum; i++) {
+            sb.append(generateMultiplicationLine(i, startNum)).append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+    public String generateMultiplicationLine(int num, int startNum) {
+        StringBuilder sb = new StringBuilder();
+        for (int j = startNum; j <= num; j++) {
+            sb.append(j).append("*").append(num).append("=").append(num * j);
+            if (j != num) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
 }
